1e4367e9750a607f25bd61f4335561935164ca1a
Anastasia Tarasova <anastasia@anastasias-macbook-pro.local>
2016-05-23 19:32:31 +0300

    Initial Commit

---
+++ RenderOpenCV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RenderOpenCV
+//
+//  Created by Anastasia Tarasova on 23/05/16.
+//  Copyright © 2016 Anastasia Tarasova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
